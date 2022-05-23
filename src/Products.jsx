4f54a8cd9d4e85845aa0113419ae20aff2a79e75
Alex <bertoncin@bludata.com>
2022-05-23 11:59:36 +0200

    4.5 URL Placeholder

--- src/Products.jsx
+++ src/Products.jsx
@@ -2,12 +2,14 @@ import React, {useState, useEffect} from "react";
 import "./App.css";
 import Spinner from "./Spinner";
 import useFetch from "./services/useFetch";
+import { useParams } from "react-router-dom";
 
 export default function Products() {
   const [size, setSize] = useState("");
+  const { category } = useParams();
 
   const { data: products, loading, error } = useFetch(
-    "products?category=shoes"
+    "products?category=" + category
   );
 
 
