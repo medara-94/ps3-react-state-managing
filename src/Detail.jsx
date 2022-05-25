1f9805f9d06ae2e066c0be2e16a260f0b9ee7141
Alex <bertoncin@bludata.com>
2022-05-25 14:09:02 +0200

    5.2 Creating a Shoe Size Select

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
@@ -8,6 +8,7 @@ export default function Detail() {
   //leggo dall'url  
   const {id} = useParams();
   const navigate = useNavigate();  
+  const [sku, setSku] = useState("");
   const { data: product, loading, error } = useFetch(`products/${id}`);
 
   if (loading) 
@@ -23,6 +24,13 @@ export default function Detail() {
        <h1>{product.name}</h1>
        <p>{product.description}</p>
             <p id="price">${product.price}</p>
+            <select 
+                id="size" 
+                value = {sku} 
+                onChange={(event) => setSku(event.target.value) }>
+                <option value="">What size?</option>
+                    {product.skus.map((s) => (<option key={s.sku} value={s.sku}>{s.size}</option>))}
+            </select>
             <p>
                 <button className="btn btn-primary" onClick={()=>navigate("/cart")}>
                     Add to cart
