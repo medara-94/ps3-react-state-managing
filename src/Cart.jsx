f71e06b1c3d407ffd587cc8ff555f5c74fd727f1
Alex <bertoncin@bludata.com>
2022-05-25 16:10:28 +0200

    5.12 Deriving State for Cart Header

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -42,9 +42,14 @@ export default function Cart({ cart, updateQuantity }) {
   if (loading) return <Spinner />;
   if (error) throw error;
 
+  //array.reduce esegue a reduce function (da fornire) on each elements dell'array
+  // e ritorna un singolo valore
+  const numItemsInCart = cart.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <section id="cart">
-      <h1>Cart</h1>
+      <h1> {numItemsInCart === 0 ? "Your cart is empty" 
+             : `${numItemsInCart} Item${numItemsInCart>1 ? "s" : ""} in My Cart` } </h1>
       <ul>{cart.map(renderItem)}</ul>
     </section>
   );
