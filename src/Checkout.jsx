9d4253073cabd6df5fb13eb31b6695ec39f295d5
Alex <bertoncin@bludata.com>
2022-05-26 16:57:35 +0200

    6.6 State Enum implement

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
 
+const STATUS = {
+  IDLE: "IDLE",
+  SUBMITTED: "SUBMITTED",
+  SUBMITTING: "SUBMITTING",
+  COMPLETED: "COMPLETED",
+}
+
 // Declaring outside component to avoid recreation on each render
 const emptyAddress = {
   city: "",
@@ -8,6 +15,7 @@ const emptyAddress = {
 
 export default function Checkout({ cart }) {
   const [address, setAddress] = useState(emptyAddress);
+  const [status, setStatus] = useState(STATUS.IDLE);
 
   function handleChange(e) {
     e.persist(); //Il garbage collector di React potrebbe pulire l'evento prima di questo momento
@@ -26,7 +34,8 @@ export default function Checkout({ cart }) {
   }
 
   async function handleSubmit(event) {
-    // TODO
+    event.preventDefault();
+    setStatus(STATUS.SUBMITTING);
   }
 
   return (
@@ -67,6 +76,7 @@ export default function Checkout({ cart }) {
             type="submit"
             className="btn btn-primary"
             value="Save Shipping Info"
+            disable={status === STATUS.SUBMITTING}
           />
         </div>
       </form>
