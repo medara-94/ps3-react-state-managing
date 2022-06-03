c3ca8a2be38390328969d32c8bca9753dfc087fd
Alex <bertoncin@bludata.com>
2022-06-03 15:24:44 +0200

    8.7 Consuming Reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -8,18 +8,19 @@ import Detail from "./Detail";
 //import Detail from "./DetailRefs";
 import Cart from "./Cart";
 import Checkout from "./Checkout";
+import cartReducer from "./cartReducer";
+
+let initialCart;
+try {
+  initialCart = JSON.parse(localStorage.getItem("cart")) ?? []; //?? coalesce operator
+} catch {
+  console.log("The cart could not be parsed into JSON");
+  initialCart = [];
+}
 
 export default function App() {
-  // Mettendo una funzione sulla chiamata dello use state viene chiamata solo una volta
-  // al caricamento iniziale
-  const [cart, setCart] = useState(() => {
-    try {
-      return JSON.parse(localStorage.getItem("cart")) ?? []; //?? coalesce operator
-    } catch {
-      console.log("The cart could not be parsed into JSON");
-      return [];
-    }
-  });
+  // const [state, dispatch] = useReducer(reducer, initArg);
+  const [cart, dispatchCart] = useReducer(cartReducer, initialCart);
 
   // Says: anytime cart changes, store it in localStorage as a JSON String
   useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
@@ -34,15 +35,15 @@ export default function App() {
             <Route path="/:category" element={<Products />} />
             <Route
               path="/:category/:id"
-              element={<Detail addToCart={addToCart} />}
+              element={<Detail dispatchCart={dispatchCart} />}
             />
             <Route
               path="/cart"
-              element={<Cart cart={cart} updateQuantity={updateQuantity} />}
+              element={<Cart cart={cart} dispatchCart={dispatchCart} />}
             />
             <Route
               path="/checkout"
-              element={<Checkout cart={cart} emptyCart={emptyCart} />}
+              element={<Checkout cart={cart} dispatchCart={dispatchCart} />}
             />
           </Routes>
         </main>
