71355c9833a3c5f3dd7a9ae52ef3a717279f028b
Alex <bertoncin@bludata.com>
2022-06-03 16:37:09 +0200

    9.6 Create a Provider Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -8,27 +8,11 @@ import Detail from "./Detail";
 //import Detail from "./DetailRefs";
 import Cart from "./Cart";
 import Checkout from "./Checkout";
-import cartReducer from "./cartReducer";
-import { CartContext } from "./cartContext";
-
-let initialCart;
-try {
-  initialCart = JSON.parse(localStorage.getItem("cart")) ?? []; //?? coalesce operator
-} catch {
-  console.log("The cart could not be parsed into JSON");
-  initialCart = [];
-}
 
 export default function App() {
-  // const [state, dispatch] = useReducer(reducer, initArg);
-  const [cart, dispatchCart] = useReducer(cartReducer, initialCart);
-
-  // Says: anytime cart changes, store it in localStorage as a JSON String
-  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
-
   return (
     //value props determines what will be shared via the context
-    <CartContext.Provider value={{ cart, dispatchCart }}>
+    <>
       <div className="content">
         <Header />
         <main>
@@ -48,6 +32,6 @@ export default function App() {
         </main>
       </div>
       <Footer />
-    </CartContext.Provider>
+    </>
   );
 }
--- src/cartContext.js
+++ src/cartContext.js
@@ -1,4 +1,23 @@
-import React from "react";
+import React, { useReducer, useEffect } from "react";
+import cartReducer from "./cartReducer";
 
 // The default value would apply if a component tries consuming the context without a provider in a parent
 export const CartContext = React.createContext(null);
+
+let initialCart;
+try {
+  initialCart = JSON.parse(localStorage.getItem("cart")) ?? []; //?? coalesce operator
+} catch {
+  console.log("The cart could not be parsed into JSON");
+  initialCart = [];
+}
+
+export function CartProvider(props) {
+    // const [state, dispatch] = useReducer(reducer, initArg);
+    const [cart, dispatchCart] = useReducer(cartReducer, initialCart);
+    // Says: anytime cart changes, store it in localStorage as a JSON String
+    useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
+
+    const contextValue = { cart, dispatchCart};    
+    return <CartContext.Provider value={contextValue}>{props.children}</CartContext.Provider>
+}
