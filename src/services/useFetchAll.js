96302c07dc45affdfc98fb9501f89d0426fdd670
Alex <bertoncin@bludata.com>
2022-06-03 14:45:03 +0200

    7.6 Store a previous value

--- src/services/useFetchAll.js
+++ src/services/useFetchAll.js
@@ -1,11 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function useFetchAll(urls) {
+  const preUrlsRef = useRef([]); // Salvo gli url già renderizzati, così da evitare un successivo render inutile
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    // Only run if the array of URLs passed in changes
+    if (areEqual(preUrlsRef.current, urls)) {
+      setLoading(false);
+      return;
+    };
+    preUrlsRef.current = urls ;
+
     const promises = urls.map((url) =>
       fetch(process.env.REACT_APP_API_BASE_URL + url).then((response) => {
         if (response.ok) return response.json();
@@ -21,7 +29,15 @@ export default function useFetchAll(urls) {
       })
       .finally(() => setLoading(false));
     // eslint-disable-next-line
-  }, []);
+  }, [urls]);
 
   return { data, loading, error };
+}
+
+// Le funzioni JS pure meglio metterle fuori dal Custom hook, così da non riallocarle ad ogni render, piccolo miglioramento delle performance
+function areEqual(array1, array2){
+  return (
+    array1.length === array2.length &&
+    array1.every((value, index) => value === array2[index])
+  );
 }
